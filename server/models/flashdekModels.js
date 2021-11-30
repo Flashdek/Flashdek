7d39444c7ea5e6def640c1f9b70a49387981374d
Tu Pham <toopham@gmail.com>
2021-11-30 12:59:02 -0800

    setting up mongodb

--- server/models/flashdekModels.js
+++ server/models/flashdekModels.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://admin:<admin>@flashdek.g467u.mongodb.net/flashdek?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://admin:admin@flashdek.g467u.mongodb.net/flashdek?retryWrites=true&w=majority';
 console.log('RUNNING ');
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
