b8bde9fa4aa69592b5414f74c3102ac2d372e9cc
chrisjtang <chrisjtang@gmail.com>
2021-11-30 20:12:02 -0800

    added frontend addquestion functionality fetch request to addquestions to the database
    co-authored-by: Garrett Hickman 79604590+grhhhh@users.noreply.github.com
    co-authored-by: Robert Maeda rob.maeda3@gmail.com

--- src/components/AddQuestion.jsx
+++ src/components/AddQuestion.jsx
@@ -1,6 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AddQuestion = () => {
+
+  const [ question, setQuestion ] = useState('');
+  const [ answer, setAnswer ] = useState('');
+
+  const technologyTypes = [
+    'react',
+    'redux',
+    'node',
+    'express',
+    'SQL',
+    'mongo',
+    'webpack'
+  ]
+  const [checkedState, setCheckedState] = useState(
+    new Array(technologyTypes.length).fill(false)
+  );
+
+  const techCheckboxes = [];
+
+  const handleChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+    index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  };
+
+  for (let i = 0; i < technologyTypes.length; i++) {
+    const tech = technologyTypes[i];
+    techCheckboxes.push(
+      <div>
+        <input type='checkbox' name={tech} index={i} value={tech} onChange={() => handleChange(i)}/>
+        <label for={tech}>{
+
+          tech
+        }</label>
+      </div>
+    )
+  }
+
+  const submitHandler = () => {
+    const arrayToSend = [];
+    for (let i = 0; i < technologyTypes.length; i++) {
+      if (checkedState[i]) {
+        arrayToSend.push(technologyTypes[i]);
+      }
+    }
+
+    console.log('question', question);
+    console.log('answer', answer);
+    console.log('arrayToSend', arrayToSend);
+
+    fetch('http://localhost:3000/api/submitCard', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/JSON',
+      },
+      body: JSON.stringify({
+        keyword: arrayToSend,
+        question: question,
+        answer: answer
+      })
+    })
+      .then(() => {
+        const updatedCheckedState = checkedState.map((item, index) =>
+          index === position ? !item : item
+          );
+        setCheckedState(updatedCheckedState);
+        setQuestion('');
+        setAnswer('');
+        alert('Your question has been submitted!');
+      })
+  }
+
   return (
     <div id="questionInput">
       <div className="row">
@@ -8,7 +81,9 @@ const AddQuestion = () => {
           <div className="categories">Categories/Keywords</div>
         </div>
         <div className="column">
-          <input></input>
+          <div id='checkboxes'>
+            {techCheckboxes}
+          </div>
         </div>
       </div>
       <div className="row">
@@ -16,7 +91,7 @@ const AddQuestion = () => {
           <div className="question">Question</div>
         </div>
         <div className="column">
-          <textarea rows='4' cols='30'></textarea>
+          <textarea rows='4' cols='30' value={question} onChange={e => setQuestion(e.target.value)}></textarea>
         </div>
       </div>
       <div className="row">
@@ -24,10 +99,10 @@ const AddQuestion = () => {
           <div className="answer">Answer</div>
         </div>
         <div className="column">
-          <textarea rows='4' cols='30'></textarea>
+          <textarea rows='4' cols='30' value={answer} onChange={e => setAnswer(e.target.value)}></textarea>
         </div>
       </div>
-      <button id="submit">Submit</button>
+      <button id="submit" onClick={submitHandler}>Submit</button>
     </div>
   );
 }
