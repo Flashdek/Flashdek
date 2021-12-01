35ed88b2087b35690047636d293ccb70753cd4e1
Robert Maeda <robmaeda@Roberts-MacBook-Air.local>
2021-11-30 18:38:56 -0800

    finished AddQuestion component

--- src/components/AddQuestion.jsx
+++ src/components/AddQuestion.jsx
@@ -3,18 +3,29 @@ import React from 'react';
 const AddQuestion = () => {
   return (
     <div id="questionInput">
-      Add Question component
-      <div className="categories">
-        Categories/Keywords
-        <input></input>
+      <div className="row">
+        <div className="column">
+          <div className="categories">Categories/Keywords</div>
+        </div>
+        <div className="column">
+          <input></input>
+        </div>
       </div>
-      <div className="question">
-        Question
-        <textarea rows='4' cols='30'></textarea>
+      <div className="row">
+        <div className="column">
+          <div className="question">Question</div>
+        </div>
+        <div className="column">
+          <textarea rows='4' cols='30'></textarea>
+        </div>
       </div>
-      <div className="answer">
-        Answer
-        <textarea rows='4' cols='30'></textarea>
+      <div className="row">
+        <div className="column">
+          <div className="answer">Answer</div>
+        </div>
+        <div className="column">
+          <textarea rows='4' cols='30'></textarea>
+        </div>
       </div>
       <button id="submit">Submit</button>
     </div>
